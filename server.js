573b9d7b8f5f0153ad570e5bdad28bfbf99c443a
soad <soadul@inspir8bd.com>
2026-04-28 17:16:39 +0600

    Increased JSON body size limit to 10MB

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
